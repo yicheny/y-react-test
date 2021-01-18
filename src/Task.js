1711c596fb5dbbc2fdb0e2b0eacec0daa83ad754
YLF <1463020554@qq.com>
2021-01-19 07:41:28 +0800

    add 测试props多层传递

---
+++ src/Task.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Task = (props) => (
+    <li id={props.id}>{props.name}</li>
+);
+
+export default Task;
--- src/TodoList.js
+++ src/TodoList.js
@@ -1,13 +1,16 @@
 import React from 'react';
+import Task from "./Task.js";
 
 const ToDoList = (props) => {
     return (
         <ul>
-            {props.tasks.map((taskName, index) => (
-                <li key={index}>{taskName}</li>
-            ))}
+            {
+                props.tasks.map(task =>
+                    <Task key={task.id} id={task.id} name={task.name}/>
+                )
+            }
         </ul>
-    );
+    )
 };
 
 export default ToDoList;

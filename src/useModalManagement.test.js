a83c0aeea621a302e62d4c3fe076b84e3f722ffb
yicheny <1463020554@qq.com>
2021-01-20 16:01:31 +0800

    测试自定义hook

---
+++ src/useModalManagement.test.js
@@ -0,0 +1,23 @@
+import useModalManagement from './useModalManagement';
+import { renderHook, act } from '@testing-library/react-hooks';
+
+describe('The useModalManagement hook', () => {
+    it('should not throw an error', () => {
+        renderHook(() => useModalManagement());
+    });
+
+    it('should describe a closed modal by default', () => {
+        const { result } = renderHook(() => useModalManagement());
+        expect(result.current.isModalOpened).toBe(false);
+    });
+
+    describe('when the openModal function is called', () => {
+        it('should describe an opened modal', () => {
+            const { result } = renderHook(() => useModalManagement());
+            act(() => {
+                result.current.openModal();
+            });
+            expect(result.current.isModalOpened).toBe(true);
+        });
+    });
+});
